41a4a2e8623755e850fd1b9d82be9b52d8b98160
ImGozie <153894673+ImGozie@users.noreply.github.com>
2024-11-05 00:02:54 +0700

    f

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -1,8 +1,3 @@
     </main>
-    <footer class="bg-gray-800 text-white mt-10">
-        <div class="container mx-auto p-4 text-center">
-            <p>&copy; <?= date('Y'); ?> My Project. All rights reserved.</p>
-        </div>
-    </footer>
 </body>
 </html>
