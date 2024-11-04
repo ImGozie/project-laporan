bc7ff9df51d428c5d272f57b2e253e486995753a
ImGozie <153894673+ImGozie@users.noreply.github.com>
2024-11-05 00:02:38 +0700

    sidebar

---
+++ app/Views/components/sidebar.php
@@ -0,0 +1,36 @@
+<aside class="bg-[#FAF6F0] h-screen w-64 drop-shadow-md">
+    <div class="flex w-full h-16 bg-[#F4DFC8] justify-center shadow-sm items-center font-semibold">
+        ASU
+    </div>
+    <nav class=" mt-6 px-3">
+        <ul class="flex flex-col gap-2">
+            <li>
+                <a href="#" class="duration-300 flex hover:bg-[#000] hover:text-[#FAF6F0] items-center py-3 px-4 gap-4 rounded-md">
+                    <div class="">
+                        <i class="fa-solid fa-chart-simple"></i>
+                    </div>
+                    <p>Dashboard</p>
+                    <div class="h-2 w-2"></div>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="duration-300 flex hover:bg-[#000] hover:text-[#FAF6F0] items-center py-3 px-4 gap-4 rounded-md">
+                    <div class="">
+                        <i class="fa-solid fa-users"></i>
+                    </div>
+                    <p>User</p>
+                    <div class="h-2 w-2"></div>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="duration-300 flex hover:bg-[#000] hover:text-[#FAF6F0] items-center py-3 px-4 gap-4 rounded-md">
+                    <div class="">
+                        <i class="fa-solid fa-database"></i>
+                    </div>
+                    <p>Master Data</p>
+                    <div class="h-2 w-2"></div>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</aside>
