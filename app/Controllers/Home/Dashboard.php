c711dfb35cda4940cd579b762683c6a76a52c0ef
ImGozie <153894673+ImGozie@users.noreply.github.com>
2024-11-05 23:32:30 +0700

    update array menu dashboard

--- app/Controllers/Home/Dashboard.php
+++ app/Controllers/Home/Dashboard.php
@@ -9,6 +9,19 @@ class Dashboard extends BaseController
 {
     public function index()
     {
-        return view('Home/Dashboard');
+        $dataMenu = $this->menuJson();
+        $data = [
+            'menu' => json_encode($dataMenu),
+        ];
+        return view('Home/Dashboard', $data);
+    }
+
+    public function menuJson()
+    {
+        return [
+            ['id' => 1, 'name' => 'dashboard', 'icon' => 'fa-chart-simple', 'url' => '/home'],
+            ['id' => 2, 'name' => 'master data', 'icon' => 'fa-database', 'url' => '/about'],
+            ['id' => 3, 'name' => 'user / siswa', 'icon' => 'fa-users', 'url' => '/contact']
+        ];
     }
 }
