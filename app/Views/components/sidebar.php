f48a3ec0a45d5419d47ea5e9a3dcdd0b57275f53
DESKTOP-L4Q5CDK\Hyperdata 1 <ghoziwkwkwk21@gmail.com>
2024-11-05 16:51:51 +0700

    npm flowbite

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -5,7 +5,12 @@
                 <i class="fa-solid fa-graduation-cap !text-lg"></i>
             </div>
             <!-- <h1 class="uppercase tracking-tight font-bold">Alumni Tracker.</h1> -->
-             <button @click="expanded = !expanded" class="bg-black text-[#F4DFC8] h-9 w-9 flex items-center justify-center rounded-md absolute right-2">
+             <button 
+                data-tooltip-target="tooltip-right" 
+                data-tooltip-placement="right"
+                @click="expanded = !expanded" 
+                class="bg-black text-[#F4DFC8] h-9 w-9 flex items-center justify-center rounded-md absolute right-2"
+            >
                 <i x-bind:class="expanded ? '-scale-100' : 'scale-100'" class="duration-200 fa-solid fa-bars-staggered fa-sm"></i>
              </button>
         </div>
@@ -14,45 +19,52 @@
         <ul class="flex flex-col gap-2 *:text-gray-900 *:text-sm">
             <li>
                 <a 
-                    href="#" 
-                    class="duration-300 transition-all transition-radius flex items-center py-3 px-3 gap-4 rounded-lg"
-                    x-bind:class="expanded ? 'hover:bg-[#F4DFC8]' : 'bg-[#F4DFC8] hover:bg-black hover:text-[#F4DFC8] hover:rounded-2xl'"
+                href="#" 
+                class="group relative duration-300 transition-all transition-radius flex items-center py-3 px-3 gap-4 rounded-lg"
+                x-bind:class="expanded ? 'hover:bg-[#F4DFC8]' : 'bg-[#ffffff] hover:bg-[#F4DFC8] hover:rounded-2xl'"
                 >
                     <div class="icon-menu">
                         <i class="fa-solid fa-chart-simple"></i>
                     </div>
                     <p x-bind:class="expanded ? '' : 'max-w-0'" class="duration-150 overflow-hidden whitespace-pre">Dashboard</p>
+                    <div x-bind:class="expanded ? 'opacity-0' : 'opacity-100'" class="duration-200 absolute w-2 h-2.5 group-hover:h-8 bg-black rounded-full top-1/2 -translate-y-1/2 -left-3.5 group-hover:-left-3"></div>
                 </a>
             </li>
             <li>
                 <a 
                     href="#" 
-                    class="duration-300 transition-all transition-radius flex items-center py-3 px-3 gap-4 rounded-lg"
-                    x-bind:class="expanded ? 'hover:bg-[#F4DFC8]' : 'bg-[#F4DFC8] hover:bg-black hover:text-[#F4DFC8] hover:rounded-2xl'"
+                    class="group relative duration-300 transition-all transition-radius flex items-center py-3 px-3 gap-4 rounded-lg"
+                    x-bind:class="expanded ? 'hover:bg-[#F4DFC8]' : 'bg-[#ffffff] hover:bg-[#F4DFC8] hover:rounded-2xl'"
                 >
                     <div class="icon-menu">
                         <i class="fa-solid fa-database"></i>
                     </div>
                     <p x-bind:class="expanded ? '' : 'max-w-0'" class="duration-150 overflow-hidden whitespace-pre">Master Data</p>
+                    <div x-bind:class="expanded ? 'opacity-0' : 'opacity-100'" class="duration-200 absolute w-2 h-2.5 group-hover:h-8 bg-black rounded-full top-1/2 -translate-y-1/2 -left-3.5 group-hover:-left-3"></div>
                 </a>
             </li>
             <li>
                 <a 
                     href="#" 
-                    class="duration-300 transition-all transition-radius flex items-center py-3 px-3 gap-4 rounded-lg"
-                    x-bind:class="expanded ? 'hover:bg-[#F4DFC8]' : 'bg-[#F4DFC8] hover:bg-black hover:text-[#F4DFC8] hover:rounded-2xl'"
+                    class="group relative duration-300 transition-all transition-radius flex items-center py-3 px-3 gap-4 rounded-lg"
+                    x-bind:class="expanded ? 'hover:bg-[#F4DFC8]' : 'bg-[#ffffff] hover:bg-[#F4DFC8] hover:rounded-2xl'"
                 >
                     <div class="icon-menu">
                         <i class="fa-solid fa-users"></i>
                     </div>
                     <p x-bind:class="expanded ? '' : 'max-w-0'" class="duration-150 overflow-hidden whitespace-pre">User / Siswa</p>
+                    <div x-bind:class="expanded ? 'opacity-0' : 'opacity-100'" class="duration-200 absolute w-2 h-2.5 group-hover:h-8 bg-black rounded-full top-1/2 -translate-y-1/2 -left-3.5 group-hover:-left-3"></div>
                 </a>
             </li>
         </ul>
     </nav>
     <div class="absolute p-1 bottom-0 w-full h-16">
         <section x-bind:class="expanded ? 'bg-[#F4DFC8]' : 'bg-transparent'" class="flex h-full rounded-lg px-1.5 py-2 gap-2 drop-shadow-sm">
-            <div class="img-container h-10 w-10 flex-shrink-0 p-0.5 border-[1px] border-stone-400 rounded-full bg-white">
+            <div 
+                @click="if (!expanded) expanded = true" 
+                x-bind:class="expanded ? '' : 'cursor-pointer'"
+                class="img-container h-10 w-10 flex-shrink-0 p-0.5 border-[1px] border-stone-400 rounded-full bg-white"
+            >
                 <div class="rounded-full overflow-hidden">
                     <img src="<?= session()->get('profile') ? session()->get('profile') : 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y'; ?>" alt="User Profile">
                 </div>
