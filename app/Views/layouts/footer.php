f73d49d2869ece75b2dce9ea3ef10a2800912449
DESKTOP-L4Q5CDK\Hyperdata 1 <ghoziwkwkwk21@gmail.com>
2024-11-06 16:21:44 +0700

    footer

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -1,4 +1,5 @@
     </main>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="<?= base_url('js/alpine.js') ?>" defer></script>
 <script src="<?= base_url('js/flowbite.js') ?>"></script>
 </body>
