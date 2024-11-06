9d3b035e445a9426a46b4204d8926c0e055c0eff
DESKTOP-L4Q5CDK\Hyperdata 1 <ghoziwkwkwk21@gmail.com>
2024-11-06 16:21:49 +0700

    header update

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -4,15 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TEST</title>
-    <link href="<?= base_url('styles/style.css'); ?>" rel="stylesheet">
-    <link href="https://unpkg.com/flowbite@1.4.1/dist/flowbite.min.css" rel="stylesheet">
-<script src="https://unpkg.com/flowbite@1.4.1/dist/flowbite.min.js"></script>
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
-        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>ASSSSUUUUUUUUU</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link href="<?= base_url('styles/flowbite.css'); ?>" rel="stylesheet">
+    <!-- <link href="<?= base_url('styles/datatable.css'); ?>" rel="stylesheet"> -->
+    <link href="https://unpkg.com/flowbite@1.4.1/dist/flowbite.min.css" rel="stylesheet">
+    <!-- <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
         @import url("https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
 
@@ -28,9 +26,17 @@
             line-height: 20px;
             text-align: center;
         }
+
+        tbody tr:nth-child(odd) {
+            background-color: #ffffff;
+        }
+        
+        tbody tr:nth-child(even) {
+            background-color: #fdfdfd;
+        }
     </style>
 </head>
 
-<body class="flex">
+<body class="flex overflow-hidden">
     <?= $this->include('components/sidebar'); ?>
-    <main>
+    <main class="w-full">
