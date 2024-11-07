3e982bb1e61032d7b219bff19566fb667e394b62
DESKTOP-L4Q5CDK\Hyperdata 1 <ghoziwkwkwk21@gmail.com>
2024-11-07 14:59:44 +0700

    update import cdn

---
+++ app/Views/layouts/import.php
@@ -0,0 +1,17 @@
+<!-- Tailwind CDN -->
+<script src="https://cdn.tailwindcss.com"></script>
+<!-- Flowbite CSS -->
+<link href="<?= base_url('styles/flowbite.css'); ?>" rel="stylesheet">
+<!-- Flowbite Tooltip (Arrow) -->
+<link href="https://unpkg.com/flowbite@1.4.1/dist/flowbite.min.css" rel="stylesheet">
+<!-- Flowbite Tooltip-->
+<script src="<?= base_url('js/flowbite.js') ?>"></script>
+<!-- Font awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<!-- Datatable Styling -->
+<link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.css" />
+  
+<!-- JQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- Alpine JS Lib -->
+<script src="<?= base_url('js/alpine.js') ?>" defer></script>
