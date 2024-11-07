4a3ba885764db25a2585b2c5979d38b2cb0d612c
ImGozie <153894673+ImGozie@users.noreply.github.com>
2024-11-08 00:10:20 +0700

    footer

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -1,4 +1,29 @@
     </main>
     <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('#usersTable').DataTable({
+                ajax: {
+                    url: '<?= site_url('users/datatable'); ?>',
+                    type: 'get'
+                },
+                columns: [
+                    {
+                        data: null,
+                        render: function(data, type, row, meta) {
+                            return meta.row + meta.settings._iDisplayStart + 1;
+                        }
+                    },
+                    { data: 'username' },
+                    { data: 'email' },
+                    { data: 'password' },
+                    { data: 'login_method' },
+                    { data: 'role' }
+                ],
+                processing: true,
+                serverSide: true
+            });
+        });
+    </script>
 </body>
 </html>
