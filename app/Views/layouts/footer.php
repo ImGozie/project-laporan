569bbfb23df62f8b36b3391a934ddab7fdfa2a0a
DESKTOP-L4Q5CDK\Hyperdata 1 <ghoziwkwkwk21@gmail.com>
2024-11-07 14:58:55 +0700

    foot

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -1,6 +1,4 @@
     </main>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="<?= base_url('js/alpine.js') ?>" defer></script>
-<script src="<?= base_url('js/flowbite.js') ?>"></script>
+    <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
 </body>
 </html>
