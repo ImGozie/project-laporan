dea509d9d88df53e5ac1c573607ccba9a4dac802
ImGozie <153894673+ImGozie@users.noreply.github.com>
2024-11-08 00:10:25 +0700

    import

--- app/Views/layouts/import.php
+++ app/Views/layouts/import.php
@@ -9,7 +9,7 @@
 <!-- Font awesome -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <!-- Datatable Styling -->
-<link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.css" />
+<link rel="stylesheet" href="<?= base_url('styles/datatable.css'); ?>" />
   
 <!-- JQuery -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
