39895035d881bcd3ed802a552bd39d6c39bada9f
ImGozie <153894673+ImGozie@users.noreply.github.com>
2024-11-10 22:16:11 +0700

    user model update

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -6,7 +6,7 @@ use CodeIgniter\Model;
 
 class Users extends Model
 {
-    protected $table = 'users';
+    protected $table = 'users as a';
 
     protected $allowedFields = [
         'username',
@@ -27,6 +27,20 @@ class Users extends Model
     public function getUserByUsername($username)
     {
         return $this->where('username', $username)->first();
+    }
+
+    public function datatables()
+    {
+        return $this->db->table($this->table)
+            ->select('a.userid, a.username, a.password, a.email, a.login_method, a.role');
+    }
+
+    public function getTableConfig()
+    {
+        return [
+            'searchable' => ['username', 'email'],
+            'orderBy' => ['userid', 'username', 'email', 'password', 'login_method', 'role'],
+        ];
     }
 
     // public function getDatatables($keyword = null, $start = 0, $length = 0 , $order = null, $searchable = [], $orderBy = [])
