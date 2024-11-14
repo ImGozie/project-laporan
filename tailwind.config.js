fd2fc7c6ad3c9afdda5a2270df71a80e942e722e
ImGozie <153894673+ImGozie@users.noreply.github.com>
2024-11-14 22:16:00 +0700

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
       "./node_modules/flowbite/**/*.js",
   ],
   theme: {
-      extend: {},
+      extend: {
+          colors: {
+              'gozi': {
+                  '50': '#eff6ff',
+                  '100': '#dbeafe',
+                  '200': '#bedaff',
+                  '300': '#92c4fe',
+                  '400': '#5fa3fb',
+                  '500': '#3f83f8',
+                  '600': '#2360ed',
+                  '700': '#1b4bda',
+                  '800': '#1d3db0',
+                  '900': '#1d398b',
+                  '950': '#162455',
+              },
+          },
+      },
   },
   plugins: [
     require('flowbite/plugin')
