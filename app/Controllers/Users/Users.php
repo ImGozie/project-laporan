5c747aa406a94fbb2f21cbb7c315aa22814cc850
ImGozie <153894673+ImGozie@users.noreply.github.com>
2024-11-14 22:16:07 +0700

    u controller

--- app/Controllers/Users/Users.php
+++ app/Controllers/Users/Users.php
@@ -39,18 +39,18 @@ class Users extends BaseController
         array_map(function($row) {
             $row->action = '
                 <div class="flex whitespace-nowrap items-center justify-center">
-                    <a href="' . site_url('users/edit/' . $row->userid) . '" class="inline-flex items-center justify-center bg-yellow-500 hover:bg-yellow-600  min-w-7 min-h-8 !text-[#F4DFC8] rounded-md mr-1">
+                    <a href="' . site_url('users/edit/' . $row->userid) . '" class="inline-flex items-center justify-center  min-w-7 min-h-8 !text-gozi-950 rounded-l-lg">
                         <span>
                             <i class="fa-solid fa-pen"></i>
                         </span>
                     </a>
-                    <button class="bg-red-500 hover:bg-red-600 min-w-7 min-h-8 text-[#F4DFC8] rounded-md" data-id="' . $row->userid . '"><i class="fa-solid fa-trash mx-0.5"></i></button>
+                    <button class="min-w-7 min-h-8 text-gozi-950 rounded-r-lg" data-id="' . $row->userid . '"><i class="fa-solid fa-trash mx-0.5"></i></button>
                 </div>
             ';
             if ($row->role === 'admin') {
-                $row->role = '<span class="inline-flex bg-blue-100 text-gray-500 shadow-md min-w-14 min-h-7 items-center justify-center rounded-md">Admin</span>';
+                $row->role = '<span class="inline-flex bg-gozi-400 text-gozi-50 shadow-md min-w-14 min-h-7 items-center justify-center rounded-md">Admin</span>';
             } else {
-                $row->role = '<span class="inline-flex bg-blue-50 text-gray-500 shadow-md min-w-14 min-h-7 items-center justify-center rounded-md">User</span>';
+                $row->role = '<span class="inline-flex bg-gozi-100 text-gozi-500 shadow-md min-w-14 min-h-7 items-center justify-center rounded-md">User</span>';
             }
         }, $result['data']);
         $output = array(
