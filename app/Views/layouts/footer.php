2629b2773de3c3c77c6f8189ae39e1d10f03736d
DESKTOP-L4Q5CDK\Hyperdata 1 <ghoziwkwkwk21@gmail.com>
2024-11-15 16:40:57 +0700

    foter

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -1,67 +1,39 @@
+<div id="crud-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center !items-start w-full md:inset-0 backdrop-blur-sm h-screen max-h-full">
+    <div class="relative p-4 !top-10 w-full max-w-xl max-h-full">
+        <div class="relative bg-white rounded-lg shadow">
+            <!-- Modal Header -->
+            <div class="flex items-center justify-between p-2 md:p-3 border-b rounded-t">
+                <h3 id="modal-title" class="text-md font-medium text-gray-900"></h3>
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-toggle="crud-modal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+            <!-- Modal Body: Dynamic Content -->
+            <div id="modal-body"></div>
+        </div>
+    </div>
+</div>
 </main>
-<!-- <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="hover:text-[#F4DFC8] border-[3px] focus:bg-black focus:text-[#F4DFC8] border-black duration-150 hover:bg-black font-medium rounded-md text-sm px-5 py-2.5 text-center inline-flex items-center" type="button">
-    <p>Select Role</p>
-    <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
-    </svg>
-</button> -->
-<!-- <div id="dropdown" class="z-10 hidden rounded-lg shadow w-[140px] bg-gray-800">
-    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-      <li>
-        <a href="#" class="block px-4 py-2 hover:bg-gray-700 hover:text-white">Admin</a>
-      </li>
-    </ul>
-</div> -->
 <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
 <script>
     $(document).ready(function() {
-        $('#usersTable').DataTable({
-            ajax: {
-                url: '<?= site_url('users/datatable'); ?>',
-                type: 'get'
-            },
-            // language: {
-            //     processing: 'DataTables is currently busy'
-            // },
-            columns: [{
-                    data: null,
-                    render: function(data, type, row, meta) {
-                        return meta.row + meta.settings._iDisplayStart + 1;
-                    },
-                    className: '!text-center font-semibold'
-                },
-                {
-                    data: 'username'
-                },
-                {
-                    data: 'email'
-                },
-                {
-                    data: 'password'
-                },
-                {
-                    data: 'login_method'
-                },
-                {
-                    data: 'role'
-                },
-                {
-                    data: 'action',
-                    orderable: false,
-                    searchable: false
-                },
-            ],
-            processing: true,
-            serverSide: true,
-            order: [
-                [5, 'ASC']
-            ],
-            columnDefs: [{
-                targets: [5, 6],
-                className: '!text-center'
-            }],
+        function openModal(modalTitle, formUrl) {
+            $('#modal-title').text(modalTitle);
+
+            $('#modal-body').load(formUrl, function() {
+                $('#crud-modal').removeClass('hidden').addClass('flex');
+            });
+        }
+
+        $('.modal-trigger').on('click', function() {
+            const title = $(this).data('title');
+            const formUrl = $(this).data('url');
+
+            openModal(title, formUrl);
         });
-        $('#dt-search-0').attr('placeholder', 'Search...');
     });
 </script>
 </body>
