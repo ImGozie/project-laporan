cab7823c2f2a94471a941f2d9ab374778bda0108
DESKTOP-L4Q5CDK\Hyperdata 1 <ghoziwkwkwk21@gmail.com>
2024-11-21 15:03:18 +0700

    footer

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -3,7 +3,7 @@
         <div class="relative bg-white rounded-lg shadow">
             <!-- Modal Header -->
             <div class="flex items-center justify-between p-2 md:p-3 border-b rounded-t">
-                <h3 id="modal-title" class="text-md font-medium text-gray-900"></h3>
+                <h3 id="modal-title" class="text-md font-medium capitalize text-gray-900"></h3>
                 <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-toggle="crud-modal">
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
