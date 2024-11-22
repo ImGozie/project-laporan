64659ef5ebba185d35326a81a2980dff58544d07
DESKTOP-L4Q5CDK\Hyperdata 1 <ghoziwkwkwk21@gmail.com>
2024-11-22 16:32:57 +0700

    form

--- app/Views/users/form.php
+++ app/Views/users/form.php
@@ -1,29 +1,63 @@
 <!-- Modal body -->
-<form class="p-2 md:p-3">
+<form id="form-users" method="post" class="p-2 md:p-3">
+    <input type="hidden" name="id" value="<?= $formType == 'edit' ? $id : '' ?>">
     <div class="grid gap-2 mb-4 grid-cols-2">
         <div class="col-span-2">
             <label for="name" class="block mb-0.5 text-xs text-gray-800 after:content-['*'] after:text-red-600 after:ml-0.5">Username / Nomor Induk Siswa</label>
-            <input value="<?= $formType == 'edit' ? $user->username : '' ?>" type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-[4px] focus:!ring-gozi-400 block w-full p-2.5" placeholder="example: 22519/1990.063" required="">
+            <input value="<?= $formType == 'edit' ? $res->username : '' ?>" type="text" name="username" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-[4px] focus:!ring-gozi-400 block w-full p-2.5" placeholder="example: 22519/1990.063" required="">
         </div>
         <div class="col-span-2">
             <label for="category" class="block mb-0.5 text-xs text-gray-800 after:content-['*'] after:text-red-600 after:ml-0.5">Role</label>
-            <select id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-[4px] block w-full p-2.5 focus:!ring-gozi-400">
-                <option selected value="User">User</option>
-                <option value="Admin">Admin</option>
+            <select id="category" name="role" class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-[4px] block w-full p-2.5 focus:!ring-gozi-400">
+                <option value="user" <?= (isset($res->role) && $res->role === 'user') ? 'selected' : '' ?>>User</option>
+                <option value="admin" <?= (isset($res->role) && $res->role === 'admin') ? 'selected' : '' ?>>Admin</option>
             </select>
         </div>
         <div class="col-span-2">
             <label for="name" class="block mb-0.5 text-xs text-gray-800 after:content-['*'] after:text-red-600 after:ml-0.5">Password</label>
-            <input value="" type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-[4px] focus:!ring-gozi-400 block w-full p-2.5" placeholder="example: $2a$12$ar.A3NtxJibqJuH24vEvT" required="">
+            <input value="<?= $formType == 'edit' ? (empty($res->password) ? '-' : $res->password) : '' ?>" type="text" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-[4px] focus:!ring-gozi-400 block w-full p-2.5" placeholder="example: $2a$12$ar.A3NtxJibqJuH24vEvT" required="">
         </div>
     </div>
     <div class="flex gap-2 justify-end border-t-2 border-gray-200 pt-2 md:pt-3">
         <button class="duration-150 text-white inline-flex items-center bg-yellow-500 hover:bg-yellow-600 rounded-[4px] text-sm px-3 py-2.5 text-center">
             <i class="fa-solid fa-rotate-right fa-lg"></i>
         </button>
-        <button type="submit" class="duration-150 text-white inline-flex gap-1 items-center bg-gozi-600 hover:bg-gozi-500 rounded-[4px] text-sm px-5 py-2.5 text-center">
+        <button type="submit" class="duration-150 text-white inline-flex gap-2 items-center bg-gozi-600 hover:bg-gozi-500 rounded-[4px] text-sm px-5 py-2.5 text-center">
             <i class="fa-solid fa-plus fa-lg"></i>
-            Submit Data
+            <?= $formType == 'edit' ? 'Save Data' : 'Submit Data' ?>
         </button>
     </div>
 </form>
+<script>
+    $(document).ready(function() {
+        $("#form-users").on("submit", function(e) {
+            e.preventDefault();
+            let formType = "<?= $formType ?>";
+            let link = "<?= site_url('users/add') ?>";
+            if (formType == "edit") {
+                link = "<?= site_url('users/update') ?>";
+            }
+            let data = $(this).serialize();
+            $.ajax({
+                type: "POST",
+                url: link,
+                data: data,
+                dataType: "json",
+                success: function(response) {
+                    let notifType = response.success ? "success" : "error";
+                    // showNotif(notifType, response.message);
+                    console.log(notifType, response.message);
+                    if (response.success) {
+                        closeModal('#crud-modal');
+                        tb.ajax.reload();
+                    }
+                },
+                error: function(xhr, ajaxOptions, thrownError) {
+                    showError(thrownError + ", please contact administrator for the further");
+                },
+            });
+
+            return false;
+        });
+    });
+</script>
