c85f11b7c355f4fa7e801939fd22b0b502aab59f
DESKTOP-L4Q5CDK\Hyperdata 1 <ghoziwkwkwk21@gmail.com>
2024-11-22 16:32:42 +0700

    user model

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -19,11 +19,6 @@ class Users extends Model
         'created_date'
     ];
 
-    public function store($data)
-    {
-        return $this->insert($data);
-    }
-
     public function getUserByUsername($username)
     {
         return $this->where('username', $username)->first();
@@ -45,37 +40,23 @@ class Users extends Model
 
     public function getOne($id = '')
     {
-        $x = $this->builder;
-        if ($id != '') $x->where('id', $id);
+        $x = $this->db->table($this->table);
+        if ($id != '') $x->where('userid', $id);
         return $x->get()->getRow();
     }
-    // public function getDatatables($keyword = null, $start = 0, $length = 0 , $order = null, $searchable = [], $orderBy = [])
-    // {
-    //     $builder = $this->db->table('users');
-    //     if ($keyword) {
-    //         $spaceFilter = explode(' ', $keyword);
-    //         $columns = $searchable;
 
-    //         foreach ($spaceFilter as $filter) {
-    //             foreach ($columns as $column) {
-    //                 $builder = $builder->orLike($column, $filter);
-    //             }
-    //         }
-    //     }
-    //     if ($order && isset($order['column']) && isset($order['dir'])) {
-    //         $columns = $orderBy;
-    //         $columnIndex = $order['column'];
-    //         $direction = $order['dir'];
-    //         $builder->orderBy($columns[$columnIndex], $direction);
-    //     }
-    //     if (!empty($start) || !empty($length)) {
-    //         $builder = $builder->limit($length, $start);
-    //     }
-    //     return $builder->get()->getResult();
-    // }
+    public function store($data)
+    {
+        return $this->insert($data);
+    }
 
-    // public function countAllData()
-    // {
-    //     return $this->countAllResults();
-    // }
+    public function updateUser($id, $data)
+    {
+        $this->db->table($this->table)->where('userid', $id)->update($data);
+    }
+
+    public function destroy($id)
+    {
+        return $this->db->table($this->table)->delete(['userid' => $id]);
+    }
 }
